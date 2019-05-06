cd33bbc446f1311717c8bbbf67c819168e3f27dc
javabyraghu <javabyraghu@gmail.com>
2019-05-06 22:54:53 +0530

    Initial Commit

---
+++ src/main/java/com/app/SpringBootWebOAuth2ExApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebOAuth2ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebOAuth2ExApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.app.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+	
+
+	
+}
